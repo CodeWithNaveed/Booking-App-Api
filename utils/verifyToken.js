0b2e47e8e638a94b66a576c2e36d5e8cf1cf2bf0
naveed ali abbasi <getinfo.naveed@gmail.com>
2025-03-25 22:38:27 +0500

    set verify token

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,24 +1,13 @@
 import jwt from "jsonwebtoken";
 import { createError } from "./error.js";
 
-// export const verifyToken = (req, res, next) => {
-//     console.log("Received Cookies:", req.cookies); // 🔍 Debugging
-
-//     const token = req.cookies.access_token;
-//     if (!token) {
-//         return next(createError(401, "You are not authenticated!"));
-//     }
-
-//     jwt.verify(token, process.env.JWT, (err, user) => {
-//         if (err) return next(createError(403, "Token is not valid!"));
-//         req.user = user;
-//         next();
-//     });
-// };
-
 export const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1]; // "Bearer TOKEN"
-    if (!token) return next(createError(401, "You are not authenticated!"));
+    // console.log("Received Cookies:", req.cookies); 
+    const token = req.cookies.access_token;
+    // console.log("Received Token:", token);
+    if (!token) {
+        return next(createError(401, "You are not authenticated!"));
+    }
 
     jwt.verify(token, process.env.JWT, (err, user) => {
         if (err) return next(createError(403, "Token is not valid!"));
@@ -26,7 +15,6 @@ export const verifyToken = (req, res, next) => {
         next();
     });
 };
-
 
 export const verifyUser = (req, res, next) => {
     verifyToken(req, res, next, () => {
